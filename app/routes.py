4b75c2d158941e1c30ce68e2d239205d70128903
Your Name <you@example.com>
2024-09-18 20:46:41 +0530

    Initial commit with code2

---
+++ app/routes.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+# Other routes and application code
